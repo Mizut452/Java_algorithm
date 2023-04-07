c980845bbe93dcbd55c656930b621151874c1ae8
Mizut452 <ssvror444@gmail.com>
2023-04-07 22:39:57 +0900

    FizzBuzzを書いた

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int number= 1; number <= 100; number++) {
+            if(number % 3 == 0 && number % 5 == 0) {
+                System.out.println(number+"は"+  "FizzBuzz");
+            }
+            else if (number % 3 == 0) {
+                System.out.println(number+"は"+"Fizz");
+
+            } else if (number % 5 == 0 ) {
+                System.out.println(number+"は"+"Buzz");
+            } else {
+                System.out.println(number + "は" + "FizzでもBuzzでもないよ");
+            }
+        }
+    }
+}
