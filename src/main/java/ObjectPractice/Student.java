8d267cfcaea184b7431ba9048b1fe9c5e046a6ee
Mizut452 <ssvror444@gmail.com>
2023-04-16 23:52:39 +0900

    Studentクラスの作成

---
+++ src/main/java/ObjectPractice/Student.java
@@ -0,0 +1,54 @@
+package ObjectPractice;
+
+public class Student {
+
+    //メンバ変数
+    private int studentNumber;
+    private String studentName;
+    private int studentAge;
+
+    //コンストラクタ
+    public Student(int studentNumber, String studentName, int studentAge) {
+        this.studentNumber = studentNumber;
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+    }
+
+    //getter
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public int getStudentAge() {
+        return studentAge;
+    }
+
+    //setter
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentAge(int studentAge) {
+        this.studentAge = studentAge;
+    }
+
+    public String toString() {
+        return "生徒情報：学籍番号" + studentNumber + "名前" + studentName + "年齢" + studentAge;
+    }
+
+    public String tenYearsLater(String studentName, int studentAge) {
+        studentAge = studentAge + 10;
+        return studentName + "さんの10年後のの年齢差は" + studentAge + "歳です。";
+    }
+
+
+
+}
