59b0ab70b5d3032e8fe3bebe7b49d4b067368e63
Mizut452 <ssvror444@gmail.com>
2023-04-16 23:53:19 +0900

    Studentクラスを動かしてみる。

--- src/main/java/ObjectPractice/Main.java
+++ src/main/java/ObjectPractice/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Fish Suzuki = new Fish("Suzuki", "Blue");
         System.out.println(Suzuki.getFishColor());
         System.out.println(Suzuki.getFishName());
+
+        Student suzuki = new Student(1, "鈴木", 18);
+        Student yoshida = new Student(100, "吉田", 13);
+        System.out.println(suzuki);
+        System.out.println(yoshida.tenYearsLater(yoshida.getStudentName(), yoshida.getStudentAge()));
     }
 }
