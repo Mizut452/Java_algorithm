710f5daa7778500dc885801665acf98374457307
Mizut452 <ssvror444@gmail.com>
2023-05-04 18:54:38 +0900

    逐次検索のプログラム

---
+++ src/main/java/Algorythm/FirstAndFirst/SearchValueSequential.java
@@ -0,0 +1,24 @@
+package Algorythm.FirstAndFirst;
+
+import java.util.Scanner;
+
+public class SearchValueSequential {
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 5, 6};
+        Scanner sc = new Scanner(System.in);
+        int searchValue = sc.nextInt();
+        int findID = -1;
+
+        for (int i = 0; i < data.length; i++) {
+            if(searchValue == data[i]) {
+                findID = i;
+                break;
+            }
+        }
+        if (findID == -1) {
+            System.out.println(searchValue + "はデータから見つかりませんでした。");
+        } else {
+            System.out.println(searchValue + "はデータの" + findID +"番目に格納されています。");
+        }
+    }
+}
