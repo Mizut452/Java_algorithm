2d8e43c79abbed1fa78944ffeed1dbcf4f7b65c7
Mizut452 <ssvror444@gmail.com>
2023-05-11 22:59:16 +0900

    記念日計算

---
+++ src/main/java/Algorythm/Paiza/ProblemD/anniversary.java
@@ -0,0 +1,12 @@
+package Algorythm.Paiza.ProblemD;
+
+import java.util.Scanner;
+
+public class anniversary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        System.out.println(second - first);
+    }
+}
