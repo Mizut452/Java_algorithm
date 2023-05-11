792715e3a5012f1da8c2f5e2036b7bd365ab1392
Mizut452 <ssvror444@gmail.com>
2023-05-11 22:50:42 +0900

    コメの計算

---
+++ src/main/java/Algorythm/Paiza/D.java
@@ -0,0 +1,11 @@
+package Algorythm.Paiza;
+
+import java.util.Scanner;
+
+public class D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int riceAmount = sc.nextInt();
+        System.out.println(riceAmount * 150);
+    }
+}
