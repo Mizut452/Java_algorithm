346d4d18ef57b6376eede6041795e3ee588f8a12
Mizut452 <ssvror444@gmail.com>
2023-05-23 03:40:25 +0900

    安息の地を求めて

---
+++ src/main/java/Algorythm/Paiza/ProblemC/ReposeGround.java
@@ -0,0 +1,27 @@
+package Algorythm.Paiza.ProblemC;
+
+import java.util.Scanner;
+
+public class ReposeGround {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int constructX = 20;
+        int constructY = 10;
+        int noiseSize =  10;
+        int treeNum = 3;
+
+        for (int i = 0; i < treeNum; i++) {
+            int treeX = 25;
+            int treeY = 10;
+            double xLength = Math.pow(treeX - constructX, 2);
+            double yLength = Math.pow(treeY - constructY, 2);
+            double noisePow = Math.pow(noiseSize, 2);
+
+            if((int) xLength + (int) yLength > noisePow) {
+                System.out.println("silent");
+            } else {
+                System.out.println("noisy");
+            }
+        }
+    }
+}
