6b64b56705962037c0746a88721ffb7acc703af3
Mizut452 <ssvror444@gmail.com>
2023-05-29 05:46:33 +0900

    リストのテスト

---
+++ src/main/java/Algorythm/ArrayAndList/ArrayTest.java
@@ -0,0 +1,21 @@
+package Algorythm.ArrayAndList;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        List<String> frontaleList = new ArrayList<>();
+        frontaleList.add("上福元");
+        frontaleList.add("大島僚太");
+        frontaleList.add("家長昭博");
+        frontaleList.add("レアンドロ・ダミアン");
+        frontaleList.add("シミッチ");
+        frontaleList.add("チョンソンリョン");
+        HashMap<String,List<String>> teamMap = new HashMap<>();
+        teamMap.put("川崎フロンターレ", frontaleList);
+        System.out.println(teamMap);
+    }
+}
--- src/main/java/Algorythm/Paiza/ProblemC/DislikeNumber.java
+++ src/main/java/Algorythm/Paiza/ProblemC/DislikeNumber.java
@@ -7,23 +7,21 @@ import java.util.Scanner;
 public class DislikeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int dislikeNumber = sc.nextInt();
-        int bedroomNumber = sc.nextInt();
-        int[] bedNumList = new int[bedroomNumber];
-        for (int i = 0; i < bedroomNumber; i++) {
-            int bedNumber = sc.nextInt();
-            bedNumList[i] = bedNumber;
-        }
-
-        for (int bedNumber : bedNumList) {
-            while(bedNumber != 0) {
-                if(bedNumber % 10 != dislikeNumber) {
-                  bedNumber /= 10;
-                } else {
-
+        int dislikeNum = 4;
+        int hospitalRoom = 5;
+        List<Integer> dislikeRoomList = new ArrayList<>();
+        for (int i = 0; i < hospitalRoom; i++) {
+            String roomNum = sc.next();
+            for (int j = 0; j < roomNum.length(); j++) {
+                int roomChar = roomNum.charAt(j) - 48;
+                if(roomChar == dislikeNum) {
+                    dislikeRoomList.add(Integer.parseInt(roomNum));
+                    break;
                 }
             }
-
+        }
+        for(int dislikeRoom : dislikeRoomList) {
+            System.out.println(dislikeRoom);
         }
     }
 }
